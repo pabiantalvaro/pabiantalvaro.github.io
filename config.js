165b7fcaab9b7232f07968cebab1bb8d76d0970d
pabiantalvaro <131834564+pabiantalvaro@users.noreply.github.com>
2026-05-26 14:33:42 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,31 @@
+const dashboardConfig = {
+  "quotes": [
+    "The secret of getting ahead is getting started.",
+    "Focus on being productive instead of busy."
+  ],
+  "schedule": [
+    {"time": "00:00", "title": "Rest & Sleep"},
+    {"time": "03:00", "title": "Tahajjud"},
+    {"time": "03:30", "title": "TPA: Logika"},
+    {"time": "04:30", "title": "Rest"},
+    {"time": "05:30", "title": "MATDAS"},
+    {"time": "07:00", "title": "Breakfast"},
+    {"time": "08:00", "title": "MATDAS"},
+    {"time": "11:30", "title": "Rest"},
+    {"time": "13:00", "title": "Indonesian"},
+    {"time": "14:00", "title": "English"},
+    {"time": "14:40", "title": "Lil' Workout"},
+    {"time": "15:00", "title": "Rest"},
+    {"time": "15:40", "title": "TPA: Verbal"},
+    {"time": "16:20", "title": "TPA: Logika"},
+    {"time": "17:00", "title": "TPA: Kuantitaitf"},
+    {"time": "17:50", "title": "Rest"},
+    {"time": "19:40", "title": "TPA: Verbal"},
+    {"time": "20:20", "title": "TPA: Kuantitatif"},
+    {"time": "21:30", "title": "Rest & Sleep"}
+  ],
+  "longTermEvents": [
+    {"title": "SIMAK UI", "targetDate": "2026-06-14T00:00:00"},
+    {"title": "Pengumuman IUP ITB", "targetDate": "2026-06-04T12:00:00"}
+  ]
+};
