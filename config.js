c61a9531be5f9f2344fb64bdfbd931e74ff6c8b3
pabiantalvaro <131834564+pabiantalvaro@users.noreply.github.com>
2026-05-30 07:55:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ const dashboardConfig = {
     {"time": "07:00", "title": "Breakfast"},
     {"time": "08:00", "title": "MATDAS"},
     {"time": "11:30", "title": "Rest"},
-    {"time": "13:00", "title": "Indonesian"},
-    {"time": "14:00", "title": "English"},
-    {"time": "14:40", "title": "Lil' Workout"},
+    {"time": "13:00", "title": "Coding /Designing"},
+    {"time": "13:45", "title": "MATDAS"},
+    {"time": "14:30", "title": "Lil' Workout"},
     {"time": "15:00", "title": "Rest"},
     {"time": "15:40", "title": "TPA: Verbal"},
     {"time": "16:20", "title": "TPA: Logika"},
@@ -27,5 +27,28 @@ const dashboardConfig = {
   "longTermEvents": [
     {"title": "SIMAK UI", "targetDate": "2026-06-14T00:00:00"},
     {"title": "Pengumuman IUP ITB", "targetDate": "2026-06-04T12:00:00"}
+  ],
+  "woRoutine": [
+    {"activity": "Tricep Dips, Plank, Bicycle Crunches",
+      "muscleTarget": "Abs"
+    },
+    {"activity": "Standard, Pike, and Wide-Grip Push-ups",
+      "muscleTarget": "Chests + Shoulders"
+    }, 
+    {"activity": "Hanging Knee or Lying Leg Raises, Flutter Kicks",
+      "muscleTarget": "V-Line"
+    },
+    {"activity": "Incline + Elevated Pike + Decline Push-ups, and Crab Walks",
+      "muscleTarget": "Chests + Shoulders"
+    },
+    {"activity": "Reverse Crunches & Russian Twists",
+      "muscleTarget": "Abs"
+    },
+	{"activity": "No workout today!",
+      "muscleTarget": "<3"
+    },
+{"activity": "No workout today!",
+      "muscleTarget": "<3"
+    },
   ]
 };
